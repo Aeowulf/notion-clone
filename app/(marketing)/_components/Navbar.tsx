0336f2cfb1d5e674f4d6a76e0a10843dc7886f83
Alex Wulf <alexaethelwulf@gmail.com>
2023-10-13 21:41:12 -0500

    created and implemented ModeToggle component

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -3,6 +3,7 @@
 import { useScrollTop } from "@/hooks/use-scroll-top"
 import { cn } from '@/lib/utils'
 
+import { ModeToggle } from "@/components/ModeToggle"
 import { Logo } from "./Logo"
 
 export const Navbar = () => {
@@ -16,7 +17,7 @@ export const Navbar = () => {
       <Logo />
 
       <div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
-        Login
+        <ModeToggle />
       </div>
     </div>
   )
